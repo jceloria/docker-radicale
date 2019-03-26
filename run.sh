7d8034ee0b33e3e9236dc07acbe20aca730516b2
John Celoria <john.celoria@q2ebanking.com>
2019-03-26 06:56:07 -0500

    +git && symlinks to shared collection

--- run.sh
+++ run.sh
@@ -5,6 +5,25 @@ if [[ ! -e /data/.config/radicale/config ]]; then
     mkdir -p /data/.config && mv /etc/radicale /data/.config
 fi
 
+# setup git repository for change tracking
+if [[ ! -e /data/.git ]]; then
+    (cd /data && git init)
+    cat <<-'!!' > /data/.gitignore
+	.*_history
+	.Radicale.cache
+	.Radicale.lock
+	.Radicale.tmp.*
+	!!
+fi
+
+# create symlinks to shared calendars
+(
+  cd /data/collection-root 2>/dev/null && for user in *; do
+    [[ ${user} == shared ]] && continue
+    (cd ${user} && for cal in ../shared/*; do ln -sf ${cal} .; done)
+  done
+)
+
 # fix permissions
 chown -R ${PUID:=dav}:${PGID:=dav} /data
 
